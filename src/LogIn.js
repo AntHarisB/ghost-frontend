96e10852ad326a6e608a068e65394d9740ef7887
Nejra Rizvić <nejra.rizvic.20@size.ba>
2023-04-29 00:16:09 +0200

    login

--- src/LogIn.js
+++ src/LogIn.js
@@ -40,70 +40,70 @@ export default function Login(){
   
 
   return(
-    <div className="flex flex-col lg:flex-row min-h-screen ">
-    <div
-      className="w-full lg:w-1/2 bg-cover bg-right flex items-center justify-center"
+ <div className="flex flex-col lg:flex-row min-h-screen ">
+    <div className="w-full lg:w-1/2 bg-cover bg-right flex items-center justify-center"
       style={{ backgroundImage: `url(${bgImage})` }}>
-      <div className="flex justify-center items-center h-screen ">
+        <div className="flex justify-center items-center h-screen ">
           <img src={logo} alt="Logo" className="w-auto sm:h-8" />
-          </div>
+        </div>
     </div>
   
   
-    <div className="w-full lg:w-1/2 py-20 flex flex-col items-center justify-center">
+  <div className="w-full lg:w-1/2 py-20 flex flex-col items-center justify-center">
     <h2 className="text-3xl font-face-gsb font-semibold text-primary  text-center  mb-6">Log in</h2>
-<form className="flex justify-center items-center bg-white px-8 pt-6 pb-8 mb-4 w-full"  onChange={handleSubmit}>
-  <div >
-    <div className="mb-4">
-      <label className="block text-primary font-face-m font-medium text-16px  mb-2" htmlFor="email">
-        Username
-      </label>
-      <input
-        className="appearance-none sm:w-450  font-face-r font-normal w-full h-12 border border-tertiary border-1 rounded border-opacity-100 py-2 px-3 text-secondary placeholder-secondary-500 leading-tight focus:outline-none focus:shadow-outline"
-        id="email"
-        
-        type=""
-        placeholder="Enter your username"
-        onChange={(e)=>setUsername(e.target.value)}
-      />
-    </div>
-    <div className="mb-7">
-      <label className="block text-primary font-face-m font-medium text-16px mb-2" htmlFor="password">
-        Password
-      </label>
-      <input
-        className="appearance-none sm:w-450 font-face-r w-full h-12 font-normal border border-tertiary border-1 border-opacity-100 rounded py-2 px-3 text-secondary placeholder-secondary-500 leading-tight focus:outline-none focus:shadow-outline"
-        id="password"
-        type="password"
-        placeholder="Enter your password"
-        onChange={(e)=>setPassword(e.target.value)}
-      />
-    </div>
-
-    <button
-      className="bg-customColor sm:w-450 font-face-gsb w-full h-12 font-semibold text-white  py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      type="button"
-      onClick={getMethod}>
-      Log in
-    </button>
-
-    <div className="flex space-x-5 justify-between w-full mt-3 ">
+    <form className="flex justify-center items-center bg-white px-8 pt-6 pb-8 mb-4 w-full"  onChange={handleSubmit}>
       <div >
-        <label className="inline-flex items-center ">
-          <input type="checkbox" className="form-checkbox bg-customColor h-4 w-4 mb-1" name="remember" />
-          <span className="ml-2 mb-1 text-16px font-face-m font-medium text-primary">Remember password</span>
-        </label>
+        <div className="mb-4">
+            <label className="block text-primary font-face-m font-medium text-16px  mb-2" htmlFor="email">
+              Username
+            </label>
+              <input
+                className="appearance-none sm:w-450  font-face-r font-normal w-full h-12 border border-tertiary border-1 rounded border-opacity-100 py-2 px-3 text-secondary placeholder-secondary-500 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type=""
+                placeholder="Enter your username"
+                onChange={(e)=>setUsername(e.target.value)}
+              />
+        </div>
+
+        <div className="mb-7">
+            <label className="block text-primary font-face-m font-medium text-16px mb-2" htmlFor="password">
+              Password
+            </label>
+              <input
+                className="appearance-none sm:w-450 font-face-r w-full h-12 font-normal border border-tertiary border-1 border-opacity-100 rounded py-2 px-3 text-secondary placeholder-secondary-500 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                onChange={(e)=>setPassword(e.target.value)}
+              />
+        </div>
+
+        <button
+          className="bg-customColor sm:w-450 font-face-gsb w-full h-12 font-semibold text-white  py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          onClick={getMethod}>
+          Log in
+        </button>
+
+        <div className="flex space-x-5 justify-between w-full mt-3 ">
+          <div >
+            <label className="inline-flex items-center ">
+              <input type="checkbox" className="form-checkbox bg-customColor h-4 w-4 mb-1" name="remember" />
+              <span className="ml-2 mb-1 text-16px font-face-m font-medium text-primary">Remember password</span>
+            </label>
+          </div>
+
+              <div className="text-right" >
+                <a  href="#" className="text-customColor text-right font-face-m font-medium text-16px underline ">
+                  Forgot password?
+                </a>
+              </div>
+        </div>
       </div>
-      <div className="text-right" >
-        <a  href="#" className="text-customColor text-right font-face-m font-medium text-16px underline ">
-          Forgot password?
-        </a>
-      </div>
-    </div>
+    </form>
   </div>
-</form>
 </div>
-    </div>
   
 );
 }
